d64b0898bc068b2c812ee6a32c85e1e4b5cb36ae
Tombatron <tommy.hanks@gmail.com>
2013-07-21 12:05:41 -0400

    The spare nJinja testing project.

---
+++ nJinjaTesting/EnvironmentTests.cs
@@ -0,0 +1,31 @@
+﻿namespace nJinjaTesting
+{
+    using System.IO;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class EnvironmentTests
+    {
+        nJinja.Environment _env;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            var templatePath = Path.Combine(System.Environment.CurrentDirectory, "Templates");
+            _env = new nJinja.Environment(templatePath);
+        }
+
+        /// <summary>
+        /// The template used for the test here is a simple template that
+        /// inherits from a base template. 
+        /// </summary>
+        [Test]
+        public void CanRenderATestTemplate()
+        {
+            var result = _env.Render("testChildPage.html", new { });
+
+            Assert.That(result, Contains.Substring("Base Template"));
+            Assert.That(result, Contains.Substring("Child Template"));
+        }
+    }
+}
---
+++ nJinjaTesting/TemplateTests.cs
@@ -0,0 +1,19 @@
+﻿namespace nJinjaTesting
+{
+    using nJinja;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TemplateTests
+    {
+        [Test]
+        public void CanRenderAdhocTemplateString()
+        {
+            var template = new Template("Hello {{ recipient }}!");
+
+            var result = template.Render(new { recipient = "world" });
+
+            Assert.That(result, Is.EqualTo("Hello world!"));
+        }
+    }
+}
