9aec28a9c1f68fa94b55292fff53506da39c8fbc
Tombatron <tommy.hanks@gmail.com>
2013-07-25 20:30:33 -0400

    Added new exception class that will be used for describing exceptions that occur in the Environment class.

---
+++ nJinja/Exceptions/EnvironmentException.cs
@@ -0,0 +1,17 @@
+﻿namespace nJinja.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class EnvironmentException : Exception
+    {
+        public EnvironmentException() { }
+
+        public EnvironmentException(string message) : base(message) { }
+
+        public EnvironmentException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected EnvironmentException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
